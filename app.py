5b234245ec4f8081ef0bb9c4aa3d718c1043343e
Jen-Yen Lin <jylinor@gmail.com>
2019-03-20 01:16:53 +0800

    init

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def handle_message(event):
     Sticker_Message = StickerSendMessage(package_id=1,sticker_id=2) 
     
     #傳送圖片
-    #Image_Message = ImageSendMessage(original_content_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png',preview_image_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png')
+    Image_Message = ImageSendMessage(original_content_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png',preview_image_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png')
 
     # 傳送影片
     # Video_Message = VideoSendMessage(original_content_url='https://jylin.myqnapcloud.com/test.mp4', preview_image_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png')
@@ -233,7 +233,7 @@ def handle_message(event):
     #     )
     # )
 
-    replay_message(event,Sticker_Message)
+    replay_message(event,Image_Message)
 
  
 def replay_message(event,text):
