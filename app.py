c46304e1a3d239ca2a55ee5db33da5376ddd601a
Jen-Yen Lin <jylinor@gmail.com>
2019-04-28 23:37:40 +0800

    init

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+rom flask import Flask, request, abort
 
 import psycopg2
 
@@ -8,9 +8,9 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-   MessageEvent,TextMessage,TextSendMessage,StickerSendMessage,ImageSendMessage,TemplateSendMessage,ButtonsTemplate,PostbackTemplateAction
-)
+#from linebot.models import (
+#    MessageEvent,TextMessage,TextSendMessage,StickerSendMessage,ImageSendMessage,TemplateSendMessage,ButtonsTemplate,PostbackTemplateAction
+#)
 
 from linebot.models import *
 
@@ -77,16 +77,16 @@ def handle_message(event):
     
     
     #傳送貼圖
-    #Sticker_Message = StickerSendMessage(package_id=1,sticker_id=2) 
+    Sticker_Message = StickerSendMessage(package_id=1,sticker_id=2) 
     
-    #傳送圖片
-    #Image_Message = ImageSendMessage(original_content_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png',preview_image_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png')
+    # #傳送圖片
+    # Image_Message = ImageSendMessage(original_content_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png',preview_image_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png')
 
-    #Video_Message = VideoSendMessage(original_content_url='https://jylin.myqnapcloud.com/test.mp4', preview_image_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png')
+    # Video_Message = VideoSendMessage(original_content_url='https://jylin.myqnapcloud.com/test.mp4', preview_image_url='https://ithelp.ithome.com.tw/upload/images/20180103/20107144nFRc5tsPkp.png')
 
-    #Audio_Message = AudioSendMessage(original_content_url='https://jylin.myqnapcloud.com/test.m4a', duration=100000)
+    # Audio_Message = AudioSendMessage(original_content_url='https://jylin.myqnapcloud.com/test.m4a', duration=100000)
 
-    #Location_Message = LocationSendMessage(title='my location', address='Tainan', latitude=22.994821, longitude=120.196452)
+    # #Location_Message = LocationSendMessage(title='my location', address='Tainan', latitude=22.994821, longitude=120.196452)
 
     # Imagemap_Message = ImagemapSendMessage(
     #     base_url='https://www.kamigo.tw/assets/kamigo-c3b10dff4cdb60fa447496b22edad6c32fffde96de20262efba690892e4461e8.png#',
@@ -154,7 +154,7 @@ def handle_message(event):
     #     )
     # )
 
-     Carousel_Template = TemplateSendMessage(
+    # Carousel_Template = TemplateSendMessage(
     #     alt_text='Carousel template',
     #     template=CarouselTemplate(
     #         columns=[
@@ -226,7 +226,7 @@ def handle_message(event):
     #     )
     # )
 
-    replay_message(event,Carousel_Template)
+    replay_message(event,Sticker_Message)
 
  
 def replay_message(event,text):
