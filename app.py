4aadd940e2dd7bd29345bd8f0c0b8bfab36bd84b
Jen-Yen Lin <jylinor@gmail.com>
2019-05-20 13:19:23 +0800

    init

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-from __future__ import unicode_literals
- 
-import errno
-import os
-import sys
-import tempfile
-from argparse import ArgumentParser
- 
 from flask import Flask, request, abort
- 
+
+import psycopg2
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -15,60 +9,36 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-    SourceUser, SourceGroup, SourceRoom,
-    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
-    ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
-    CarouselTemplate, CarouselColumn, PostbackEvent,
-    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
-    ImageMessage, VideoMessage, AudioMessage,
-    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
+   MessageEvent,TextMessage,TextSendMessage,StickerSendMessage,ImageSendMessage,TemplateSendMessage,ButtonsTemplate,PostbackTemplateAction
 )
- 
+
+from linebot.models import *
+
+import os
 app = Flask(__name__)
- 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
- 
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
- 
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
- 
- 
-# function for create tmp dir for download content
-def make_static_tmp_dir():
-    try:
-        os.makedirs(static_tmp_path)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
-            pass
-        else:
-            raise
- 
- 
+
+# Channel Access Token
+line_bot_api = LineBotApi(os.environ['lineToken'])
+# Channel Secret
+handler = WebhookHandler(os.environ['lineSecret'])
+
+
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
- 
+
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
- 
+
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
- 
+
     return 'OK'
  
  
